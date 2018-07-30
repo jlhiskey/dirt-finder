b7895a7325ae546dc0d3a4e1528f3f79b37a03a8
diego-ramos130 <diego.onethreezero@gmail.com>
2018-07-30 15:24:31 -0700

    geocoder is functional within console testing

--- js/map.js
+++ js/map.js
@@ -1,3 +1,7 @@
+var geocoder; 
+var address;
+var map
+
 function initMap() {
   //hardcoded seattle as a default layback
     var seattle = {
@@ -7,6 +11,50 @@ function initMap() {
   // The map, centered at seattle
   var map = new google.maps.Map(
     document.getElementById('map'), { zoom: 7, center: seattle});
-  // The marker, positioned at Uluru
+  // The marker, positioned at Seattle
   var marker = new google.maps.Marker({ position: seattle, map: map });
+}
+
+//what this function does; inits the geocoder, to be used in codeAddress in conjunction with pinform.address 
+function geocode() {
+  var loc  = '';
+  geocoder = new google.maps.Geocoder()
+  var latlng = new google.maps.LatLng(47.6062, -122.3321);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng
+  }
+}
+geocode();
+
+//what this function does is takes the address given and geocodes it into latlong to be used for inside google api.
+//TODO: implement this for loop to populate the map with pins based on userdata/hardcoded locations.
+
+/* var marker, i;
+
+for (i = 0; i < locations.length; i++) {
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    map: map
+  });
+
+  google.maps.event.addListener(marker, 'click', (function (marker, i) {
+    return function () {
+      infowindow.setContent(locations[i][0]);
+      infowindow.open(map, marker);
+    }
+  })(marker, i)); */
+function codeAddress() {
+  //var address = document.getElementById('address').value;
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
