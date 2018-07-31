89e6013cebd3850d1400a5e913d9cb34fbd8a889
diego-ramos130 <diego.onethreezero@gmail.com>
2018-07-31 09:50:27 -0700

    created global variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
-
-// eventListener to insure user has an account
+var userList = [];
+/* // eventListener to insure user has an account
 var signIn = document.getElementById('sign-in');
 signIn.addEventListener('submit', function(event){
   event.preventDefault();
@@ -17,4 +17,43 @@ signIn.addEventListener('submit', function(event){
 //   else if(password === localStorage){
 //     return alert('Incorrect Username or Password');
 //   }
-});
+}); */
+function login(){
+  //changes user to being logged in, gives access to create pinform
+}
+
+function User(user,email,pass,company,phone){
+  this.userName = user;
+  this.userEmail = email;
+  this.userPassword = pass;
+  this.userCompany= company;
+  this.userPhoneNumber= phone;
+  this.pinform = {
+    pinName:'',
+    pinEmail: '',
+    pinAddress: '',
+    pinHaveNeed: '',
+    pinQuantity: '',
+    pinDirtType: '',
+    pinDensity: '',
+    pinAvailability: '',
+  }
+  userList.push(this);
+}
+
+  
+
+
+function userAuth(){
+  //if user accesses h/n buttons, checks if user is logged in. if, allows user to progress and calls pinform creator, if not, redirs user to creation
+}
+
+User.prototype.makePin = function() {
+  //taking in information from the page, generates a pin object with necessary fields name email address avail, within the user. pushes to their pinform array. 
+}
+
+function makeUser(){
+  //taking in information from the page, generates a User and sets it to logged in. pushes it to userList array to be stored into local storage.
+}
+
+var jim = new User('JimBob420', 'JimBob420@gmail.com', 'JimmyEatWorld', 'Boeing', '420-420-6969');
